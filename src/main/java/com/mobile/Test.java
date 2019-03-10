5520f6b68add7b73fb115c75e0164c57350df9ad
yangshuo0517 <916631542@qq.com>
2019-03-10 17:29:34 +0800

    增加了代码

--- src/main/java/com/mobile/Test.java
+++ src/main/java/com/mobile/Test.java
@@ -5,5 +5,7 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello");
         System.out.println("你好");
+
+        System.out.println("asedawd");
     }
 }
